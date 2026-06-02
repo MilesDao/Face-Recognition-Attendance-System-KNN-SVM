69b77aab03fd84f15b6fae040359eb4a5b861bd0
MilesDao <trungdao131105@gmail.com>
2026-06-02 18:06:22 +0700

    fix app error

--- app.py
+++ app.py
@@ -22,12 +22,17 @@ RTC_CONFIGURATION = RTCConfiguration(
                 "urls": [
                     "stun:stun.l.google.com:19302",
                     "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
                     "stun:stun.services.mozilla.com",
-                    "stun:stun.l.google.com:3478",
-                    "stun:stun1.l.google.com:3478",
-                    "stun:stun2.l.google.com:3478",
                 ]
+            },
+            {
+                "urls": [
+                    "turn:openrelay.metered.ca:80",
+                    "turn:openrelay.metered.ca:443",
+                    "turn:openrelay.metered.ca:443?transport=tcp"
+                ],
+                "username": "openrelayproject",
+                "credential": "openrelayproject"
             }
         ]
     }
